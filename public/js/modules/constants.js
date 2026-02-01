5bba05124b77a86ee187462f97733cade0c9a814
ebolanca <ebolanca@hotmail.com>
2026-02-01 20:10:57 +0100

    11.28 arreglo solapamiento cabeceras y limpieza CSS

--- public/js/modules/constants.js
+++ public/js/modules/constants.js
@@ -3,7 +3,7 @@
 // Variables de configuración global
 
 // Versión del cliente (Actualizar en cada despliegue)
-export const CURRENT_CLIENT_VERSION = "11.27";
+export const CURRENT_CLIENT_VERSION = "11.28";
 
 // Constantes de UI
 export const UI_CONSTANTS = {
--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 /* public/sw.js */
 // CAMBIA ESTE NOMBRE PARA FORZAR LA ACTUALIZACIÓN EN LOS MÓVILES
-const CACHE_NAME = 'rail-app-cache-v11.27';
+const CACHE_NAME = 'rail-app-cache-v11.28';
 
 const ASSETS_TO_CACHE = [
   './',
